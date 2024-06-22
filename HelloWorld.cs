750c17a2ea23551ec0880af7415b864b9b8a10d6
EestiMees69 <brentvinnal1108@gmail.com>
2024-06-22 17:31:15 +0300

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Experimental.AI;
+
+public class Code : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //output test
+        print("Hello, world!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
